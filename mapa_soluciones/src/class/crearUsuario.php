c3af440f419ef9575a10b9252776e69b795d4906
Ely <Elychirivella10@gmail.com>
2020-07-10 15:03:09 -0400

    usuario fix

--- mapa_soluciones/src/class/crearUsuario.php
+++ mapa_soluciones/src/class/crearUsuario.php
@@ -23,12 +23,12 @@
                    $stmt = $stmt->get_result();
 
                    if ($stmt->num_rows === 0) {
-                       $sql = "INSERT INTO usuarios (id_usuario, nick, email, pass, id_rol, id_hidrologica) VALUES (NULL, ? , ? , ? , ? , ? )";
+                       $sql = "INSERT INTO usuarios (id_usuario, nick, email, pass, id_rol, id_hidrologica) VALUES (NULL, ? , ? , ? , 3 , ? )";
                        
                        $db = new DB();
                        $db=$db->connection('usuarios_m_soluciones');
                        $stmt = $db->prepare($sql); 
-                       $stmt->bind_param("sssii", $this->nick , $email , $this->pass , $rol , $hidrologica);
+                       $stmt->bind_param("sssi", $this->nick , $email , $this->pass , $hidrologica);
                        $stmt->execute();
 
                         return "Usuario Creado";
